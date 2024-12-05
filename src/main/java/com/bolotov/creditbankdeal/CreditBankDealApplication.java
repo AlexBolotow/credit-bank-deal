bc4bcb6abc0247faa7a9b1237b9ada25ca4b0b5c
AlexBolotow <bolotow27@gmail.com>
2024-12-05 07:35:02 +0300

    Initial commit

---
+++ src/main/java/com/bolotov/creditbankdeal/CreditBankDealApplication.java
@@ -0,0 +1,13 @@
+package com.bolotov.creditbankdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBankDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditBankDealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bolotov/creditbankdeal/CreditBankDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolotov.creditbankdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBankDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
