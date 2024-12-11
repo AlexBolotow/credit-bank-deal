ca03c1e4ed069e23a7220a74747575088a4d2805
AlexBolotow <bolotow27@gmail.com>
2024-12-11 07:55:46 +0300

    added logging

---
+++ src/main/java/com/bolotov/creditbankdeal/aspect/logging/ExecutionTimeLoggingAspect.java
@@ -0,0 +1,28 @@
+package com.bolotov.creditbankdeal.aspect.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class ExecutionTimeLoggingAspect {
+
+    @Around("LoggingPointcuts.restLog()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info("Execution method: {}.{}. Execution time: {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                executionTime);
+
+        return proceed;
+    }
+}
---
+++ src/main/java/com/bolotov/creditbankdeal/aspect/logging/ServiceLoggingAspect.java
@@ -0,0 +1,29 @@
+package com.bolotov.creditbankdeal.aspect.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+@Slf4j
+public class ServiceLoggingAspect {
+
+    @Before("LoggingPointcuts.serviceLog()")
+    public void doBeforeService(JoinPoint jp) {
+        String className = jp.getSignature().getDeclaringTypeName();
+        String methodName = jp.getSignature().getName();
+
+        Object[] args = jp.getArgs();
+        String argsString = args.length > 0 ? Arrays.toString(args) : "METHOD HAS NO ARGUMENTS";
+
+        log.info("RUN SERVICE: SERVICE_METHOD: {}.{}. METHOD ARGUMENTS: [{}]",
+                className, methodName, argsString);
+    }
+
+}
