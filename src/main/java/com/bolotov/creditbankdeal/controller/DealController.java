2469f9f6602928fa7dc9f3f799a86b73ad651610
AlexBolotow <bolotow27@gmail.com>
2025-02-07 14:43:48 +0300

    added swagger

--- src/main/java/com/bolotov/creditbankdeal/controller/DealController.java
+++ src/main/java/com/bolotov/creditbankdeal/controller/DealController.java
@@ -10,6 +10,8 @@ import com.bolotov.creditbankdeal.service.ClientService;
 import com.bolotov.creditbankdeal.service.DealService;
 import com.bolotov.creditbankdeal.service.OfferService;
 import com.bolotov.creditbankdeal.service.StatementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,18 +29,21 @@ public class DealController {
 
     private final DealService dealService;
 
+    @Operation(summary = "Create statement", description = "Create statement and return 4 possible offers")
     @PostMapping("/statement")
     public ResponseEntity<List<LoanOfferDto>> getOffers(@RequestBody LoanStatementRequestDto requestDto) {
         List<LoanOfferDto> result = dealService.getOffers(requestDto);
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "Select offer", description = "Update statement with applied offer")
     @PostMapping("/offer/select")
     public ResponseEntity<?> selectOffer(@RequestBody LoanOfferDto loanOfferDto) {
         dealService.selectOffer(loanOfferDto);
         return ResponseEntity.ok("Offer selected successfully");
     }
 
+    @Operation(summary = "Calculate credit", description = "Calculate credit and completion of registration")
     @PostMapping("/calculate/{statementId}")
     public ResponseEntity<?> calculateCredit(@PathVariable String statementId,
                                              @RequestBody FinishRegistrationRequestDto requestDto) {
